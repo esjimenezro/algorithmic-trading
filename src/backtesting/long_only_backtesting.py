52b948cde816efe93ec7eb6f7e4339f2f57dcbfa
esteban <esjimenezro@gmail.com>
2022-04-15 00:28:42 -0500

    Modified backtesting for being able to backtest any type of long strategy

--- src/backtesting/long_only_backtesting.py
+++ src/backtesting/long_only_backtesting.py
@@ -1,4 +1,5 @@
 from src.strategy.base_long_strategy import BaseLongStrategy
+from urllib.error import HTTPError
 from itertools import product
 import pandas as pd
 import numpy as np
@@ -6,208 +7,230 @@ import numpy as np
 
 class LongOnlyBacktester:
     """
-    Class for the vectorized backtesting of simple Long-only trading
+    Class for the vectorized backtesting of Long-only trading
     strategies.
 
-    :param BaseLongStrategy strategy: strategy to evaluate.
-    symbol: str
-        ticker symbol (instrument) to be backtested
-    start: str
-        start date for data import
-    end: str
-        end date for data import
-    tc: float
-        proportional trading costs per trade
-
+    Attributes
+    ----------
+    strategy : BaseLongStrategy
+        strategy to evaluate.
+    symbol : str
+        symbol to apply the strategy to.
+    filepath : str
+        filepath to the data.
+    start : str
+        start date for data import.
+    end : str
+        end date for data import.
+    tc : float
+        proportional trading costs per trade.
+    ohlcv_data : pd.DataFrame
+        OHLCV data read from data_filepath.
+    position_data: pd.DataFrame
+        data with features and positions.
+    opt_results: pd.DataFrame
+        data with optimization results.
+    best_params: dict
+        data with features and positions.
 
     Methods
-    =======
+    -------
     get_data:
         imports the data.
-
     test_strategy:
         prepares the data and backtests the trading strategy incl. reporting (wrapper).
-
     prepare_data:
         prepares the data for backtesting.
-
     run_backtest:
         runs the strategy backtest.
-
     plot_results:
         plots the cumulative performance of the trading strategy compared to buy-and-hold.
-
     optimize_strategy:
         backtests strategy for different parameter values incl. optimization and reporting (wrapper).
-
     find_best_strategy:
         finds the optimal strategy (global maximum).
-
-
     print_performance:
         calculates and prints various performance metrics.
-
     """
 
-    def __init__(self, strategy: BaseLongStrategy, symbol, start, end, tc):
+    def __init__(self,
+                 strategy: BaseLongStrategy,
+                 symbol: str,
+                 time_frame: str,
+                 start: str,
+                 end: str,
+                 tc: float) -> None:
+        """
+        Constructor method for the LongOnlyBacktester class.
 
-        self.filepath = filepath
+        Parameters
+        ----------
+        strategy : BaseLongStrategy
+            strategy to evaluate.
+        symbol : str
+            symbol to apply the strategy to.
+        time_frame : str
+            filepath to the data.
+        start : str
+            start date for data import.
+        end : str
+            end date for data import.
+        tc : float
+            proportional trading costs per trade.
+        """
+        self.strategy = strategy
         self.symbol = symbol
+        self.time_frame = time_frame
         self.start = start
         self.end = end
         self.tc = tc
-        self.results = None
-        self.get_data()
-        self.tp_year = (self.data.Close.count() / ((self.data.index[-1] - self.data.index[0]).days / 365.25))
-        self.data = None
+        self.ohlcv_data = self.get_data()
+        self.position_data = None
+        self.opt_results = None
+        self.best_params = None
 
-    def __repr__(self):
-        return "Long_Only_Backtester(symbol = {}, start = {}, end = {})".format(self.symbol, self.start, self.end)
-
-    def get_data(self):
-        """ Imports the data.
+    def get_data(self) -> pd.DataFrame:
         """
-        raw = pd.read_csv(self.filepath, parse_dates=["Date"], index_col="Date")
-        raw = raw.loc[self.start:self.end].copy()
-        raw["returns"] = np.log(raw.Close / raw.Close.shift(1))
-        self.data = raw
+        Imports the data.
 
-    def test_strategy(self, percentiles=None, thresh=None):
+        Returns
+        -------
+        pd.DataFrame
+            OHLCV data within the specified time range.
+        """
+        dates = pd.date_range(start=pd.to_datetime(self.start).replace(day=1),
+                              end=pd.to_datetime(self.end) + pd.DateOffset(months=1),
+                              freq='MS')
+        data = []
+        for date in dates:
+            date_ = date.strftime(format="%Y-%m")
+            filepath = "https://data.binance.vision/data/spot/monthly/klines/" \
+                       f"{self.symbol}/{self.time_frame}/" \
+                       f"{self.symbol}-{self.time_frame}-{date_}.zip"
+            try:
+                data_date = pd.read_csv(
+                    filepath,
+                    usecols=range(6),
+                    names=["Timestamp", "Open", "High", "Low", "Close", "Volume"]
+                )
+            except HTTPError:
+                print(f"No data found in {filepath}.")
+                continue
+
+            data_date.index = pd.to_datetime(data_date['Timestamp'], unit='ms')
+            data_date.index.name = "Datetime"
+            data_date.drop(columns=["Timestamp"], inplace=True)
+            data.append(data_date)
+
+        raw = pd.concat(data, axis=0)
+        return raw
+
+    def test_strategy(self, parameters: dict) -> None:
         """
         Prepares the data and backtests the trading strategy incl. reporting (Wrapper).
 
         Parameters
-        ============
-        percentiles: tuple (return_perc, vol_low_perc, vol_high_perc)
-            return and volume percentiles to be considered for the strategy.
-
-        thresh: tuple (return_thresh, vol_low_thresh, vol_high_thesh)
-            return and volume thresholds to be considered for the strategy.
+        ----------
+        parameters : dict
+            parameters for the strategy.
         """
-
-        self.prepare_data(percentiles=percentiles, thresh=thresh)
+        # Generate features and positions
+        self.prepare_data(parameters=parameters)
         self.run_backtest()
 
-        data = self.results.copy()
-        data["creturns"] = data["returns"].cumsum().apply(np.exp)
-        data["cstrategy"] = data["strategy"].cumsum().apply(np.exp)
-        self.results = data
+        self.position_data["cum_returns"] = np.exp(self.position_data["returns"].cumsum())
+        self.position_data["cum_strategy"] = np.exp(self.position_data["strategy"].cumsum())
 
-        self.print_performance()
+        self.print_performance(parameters=parameters)
 
-    def prepare_data(self, percentiles, thresh):
-        """ Prepares the Data for Backtesting.
+    def prepare_data(self, parameters: dict) -> None:
         """
+        Prepares the Data for Backtesting.
 
-        data = self.data[["Close", "Volume", "returns"]].copy()
-        data["vol_ch"] = np.log(data.Volume.div(data.Volume.shift(1)))
-        data.loc[data.vol_ch > 3, "vol_ch"] = np.nan
-        data.loc[data.vol_ch < -3, "vol_ch"] = np.nan
-
-        if percentiles:
-            self.return_thresh = np.percentile(data.returns.dropna(), percentiles[0])
-            self.volume_thresh = np.percentile(data.vol_ch.dropna(), [percentiles[1], percentiles[2]])
-        elif thresh:
-            self.return_thresh = thresh[0]
-            self.volume_thresh = [thresh[1], thresh[2]]
-
-        cond1 = data.returns >= self.return_thresh
-        cond2 = data.vol_ch.between(self.volume_thresh[0], self.volume_thresh[1])
-
-        data["position"] = 1
-        data.loc[cond1 & cond2, "position"] = 0
-
-        self.results = data
+        Parameters
+        ----------
+        parameters : dict
+            parameters for the strategy.
+        """
+        features_data = self.strategy.generate_features(ohlcv_df=self.ohlcv_data)
+        self.position_data = self.strategy.define_position(
+            features_df=features_data,
+            params=parameters
+        )
 
     def run_backtest(self):
-        """ Runs the strategy backtest.
         """
-
-        data = self.results.copy()
-        data["strategy"] = data["position"].shift(1) * data["returns"]
-        data["trades"] = data.position.diff().fillna(0).abs()
-        data.strategy = data.strategy + data.trades * self.tc
-
-        self.results = data
+        Runs the strategy backtest.
+        """
+        self.position_data["strategy"] = self.position_data["position"].shift() * self.position_data["returns"]
+        self.position_data["trades"] = self.position_data["position"].diff().fillna(0).abs()
+        self.position_data["strategy"] = self.position_data["strategy"] + self.position_data["trades"] * self.tc
 
     def plot_results(self):
-        """  Plots the cumulative performance of the trading strategy compared to buy-and-hold.
         """
-        if self.results is None:
+        Plots the cumulative performance of the trading strategy
+        compared to buy-and-hold.
+        """
+        if self.position_data is None:
             print("Run test_strategy() first.")
         else:
-            title = "{} | TC = {}".format(self.symbol, self.tc)
-            self.results[["creturns", "cstrategy"]].plot(title=title, figsize=(12, 8))
+            self.position_data[["cum_returns", "cum_strategy"]].plot(title=self.symbol,
+                                                                     figsize=(12, 8))
 
-    def optimize_strategy(self, return_range, vol_low_range, vol_high_range, metric="Multiple"):
+    def optimize_strategy(self, param_ranges: dict, metric: str = "Multiple"):
         """
         Backtests strategy for different parameter values incl. Optimization and Reporting (Wrapper).
 
         Parameters
         ============
-        return_range: tuple
-            tuples of the form (start, end, step size).
-
-        vol_low_range: tuple
-            tuples of the form (start, end, step size).
-
-        vol_high_range: tuple
-            tuples of the form (start, end, step size).
+        param_ranges: dict
+            dict of ranges for the parameters.
 
         metric: str
             performance metric to be optimized (can be "Multiple" or "Sharpe")
         """
 
-        self.metric = metric
-
         if metric == "Multiple":
             performance_function = self.calculate_multiple
-        elif metric == "Sharpe":
-            performance_function = self.calculate_sharpe
+        else:
+            raise ValueError(f"Selected metric: {metric} is not currently supported.")
 
-        return_range = range(*return_range)
-        vol_low_range = range(*vol_low_range)
-        vol_high_range = range(*vol_high_range)
-
-        combinations = list(product(return_range, vol_low_range, vol_high_range))
+        combinations = list(product(*param_ranges.values()))
 
         performance = []
         for comb in combinations:
-            self.prepare_data(percentiles=comb, thresh=None)
+            self.prepare_data(parameters=dict(zip(param_ranges.keys(), comb)))
             self.run_backtest()
-            performance.append(performance_function(self.results.strategy))
+            performance.append(performance_function(self.position_data["strategy"]))
 
-        self.results_overview = pd.DataFrame(data=np.array(combinations), columns=["returns", "vol_low", "vol_high"])
-        self.results_overview["performance"] = performance
+        self.opt_results = pd.DataFrame(data=np.array(combinations), columns=param_ranges.keys())
+        self.opt_results["performance"] = performance
         self.find_best_strategy()
 
     def find_best_strategy(self):
-        """ Finds the optimal strategy (global maximum).
         """
-
-        best = self.results_overview.nlargest(1, "performance")
-        return_perc = best.returns.iloc[0]
-        vol_perc = [best.vol_low.iloc[0], best.vol_high.iloc[0]]
-        perf = best.performance.iloc[0]
-        print("Return_Perc: {} | Volume_Perc: {} | {}: {}".format(return_perc, vol_perc, self.metric, round(perf, 5)))
-        self.test_strategy(percentiles=(return_perc, vol_perc[0], vol_perc[1]))
-
-    def print_performance(self):
-        """ Calculates and prints various Performance Metrics.
+        Finds the optimal strategy (global maximum).
         """
+        best = self.opt_results.nlargest(1, "performance")
+        self.best_params = best.drop(columns=["performance"]).iloc[0].to_dict()
+        self.test_strategy(parameters=self.best_params)
 
-        data = self.results.copy()
-        strategy_multiple = round(self.calculate_multiple(data.strategy), 6)
-        bh_multiple = round(self.calculate_multiple(data.returns), 6)
+    def print_performance(self, parameters: dict):
+        """
+        Calculates and prints various Performance Metrics.
+
+        Parameters
+        ----------
+        parameters : dict
+            parameters for the strategy.
+        """
+        strategy_multiple = round(self.calculate_multiple(self.position_data["strategy"]), 6)
+        bh_multiple = round(self.calculate_multiple(self.position_data["returns"]), 6)
         outperf = round(strategy_multiple - bh_multiple, 6)
-        cagr = round(self.calculate_cagr(data.strategy), 6)
-        ann_mean = round(self.calculate_annualized_mean(data.strategy), 6)
-        ann_std = round(self.calculate_annualized_std(data.strategy), 6)
-        sharpe = round(self.calculate_sharpe(data.strategy), 6)
+        cagr = round(self.calculate_cagr(self.position_data["strategy"]), 6)
 
         print(100 * "=")
-        print("SIMPLE PRICE & VOLUME STRATEGY | INSTRUMENT = {} | THRESHOLDS = {}, {}".format(self.symbol, np.round(
-            self.return_thresh, 5), np.round(self.volume_thresh, 5)))
+        print(f"SIMPLE PRICE & VOLUME STRATEGY | INSTRUMENT = {self.symbol} | PARAMETERS = {parameters}")
         print(100 * "-")
         print("PERFORMANCE MEASURES:")
         print("\n")
@@ -217,26 +240,12 @@ class LongOnlyBacktester:
         print("Out-/Underperformance:       {}".format(outperf))
         print("\n")
         print("CAGR:                        {}".format(cagr))
-        print("Annualized Mean:             {}".format(ann_mean))
-        print("Annualized Std:              {}".format(ann_std))
-        print("Sharpe Ratio:                {}".format(sharpe))
-
         print(100 * "=")
 
-    def calculate_multiple(self, series):
+    @staticmethod
+    def calculate_multiple(series):
         return np.exp(series.sum())
 
-    def calculate_cagr(self, series):
+    @staticmethod
+    def calculate_cagr(series):
         return np.exp(series.sum()) ** (1 / ((series.index[-1] - series.index[0]).days / 365.25)) - 1
-
-    def calculate_annualized_mean(self, series):
-        return series.mean() * self.tp_year
-
-    def calculate_annualized_std(self, series):
-        return series.std() * np.sqrt(self.tp_year)
-
-    def calculate_sharpe(self, series):
-        if series.std() == 0:
-            return np.nan
-        else:
-            return self.calculate_cagr(series) / self.calculate_annualized_std(series)
--- src/strategy/base_long_strategy.py
+++ src/strategy/base_long_strategy.py
@@ -1,11 +1,24 @@
 import pandas as pd
+import numpy as np
 
 
 class BaseLongStrategy:
+    """
+    Template class for the vectorized backtesting of Long-only trading
+    strategies.
 
-    def generate_features(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df
+    Methods
+    -------
+    generate_features:
+        generates the features to be used for the strategy.
+    define_position:
+        calculates the positions for the strategy using the features.
+    """
+    def generate_features(self, ohlcv_df: pd.DataFrame) -> pd.DataFrame:
+        features_df = ohlcv_df.copy()
+        features_df["returns"] = np.log(features_df['Close'] / features_df['Close'].shift())
+        return features_df
 
-    def define_position(self, df: pd.DataFrame) -> pd.DataFrame:
-        df['position'] = 1  # Simplest buy-and-hold strategy
-        return df
+    def define_position(self, features_df: pd.DataFrame, params: dict) -> pd.DataFrame:
+        features_df['position'] = 1  # Simplest buy-and-hold strategy
+        return features_df
